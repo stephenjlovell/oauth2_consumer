9ca0a9964cc5427fb8bc49a3cb3396c3946b44ba
Steve Lovell <sjlovell34@gmail.com>
2015-03-12 23:10:49 -0400

    adjust devise permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # whitelist = ->(u) { u.permit(:username, :email, :password, :password_confirmation,
-    #                              :provider, :uid, :affiliation) }
-    # devise_parameter_sanitizer.for(:sign_up, &whitelist)
-    # devise_parameter_sanitizer.for(:sign_in, &whitelist)
-    # devise_parameter_sanitizer.for(:account_update, &whitelist)
+    whitelist = ->(u) { u.permit(:username, :email, :password, :password_confirmation,
+                                 :provider, :uid, :affiliation) }
+    devise_parameter_sanitizer.for(:sign_up, &whitelist)
+    devise_parameter_sanitizer.for(:account_update, &whitelist)
   end
 
 end
