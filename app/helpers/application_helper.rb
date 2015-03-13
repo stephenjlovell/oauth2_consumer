7062a4f651b6b30da9fa137c29593a93198d3acb
Steve Lovell <sjlovell34@gmail.com>
2015-03-13 16:49:05 -0400

    simplify flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,24 @@
 module ApplicationHelper
-  FLASH_TYPE = { success: "alert-success", error: "alert-danger", 
-                 alert: "alert-warning", notice: "alert-info" }
+  FLASH_TYPE = { success: "success", error: "danger", 
+                 alert: "warning", notice: "info" }
  
   def bootstrap_class_for(flash_type)
     FLASH_TYPE[flash_type] || flash_type.to_s
   end
      
   def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+    flash.each do |key, msg|
+      concat(content_tag(:div, msg, class: "alert alert-#{bootstrap_class_for(key)} fade in") do
         concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-        concat message
+        concat msg
       end)
     end
     nil
   end
 end 
+
+
+# <div class="alert alert-warning alert-dismissible fade in" role="alert">
+#   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+#   <strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.
+# </div>  
